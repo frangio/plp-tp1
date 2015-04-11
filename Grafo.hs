89f5a3298dc948da470435516ca14f5298ea7baa
Francisco Giordano <frangio.1@gmail.com>
2015-04-11 19:09:22 -0300

    simplifica algunas funciones de Grafo y arregla bug en union

--- Grafo.hs
+++ Grafo.hs
@@ -1,5 +1,7 @@
 module Grafo (Grafo, vacio, nodos, vecinos, agNodo, sacarNodo, agEje, lineal, union, clausura) where
 
+import qualified Data.List as List (delete, union)
+
 data Grafo a = G [a] (a -> [a])
 
 instance (Show a) => Show (Grafo a) where
@@ -9,9 +11,9 @@ instance (Show a) => Show (Grafo a) where
 -- ---------------------------------Sección 3--------- Grafos ---------------------------
 
 -- Ejercicio 1
--- Crea nu Grafo con 
---		- ningun nodo ([]),
---		- una funcion de vecindad que a cualquier posible nodo asocia una lista vacia de vecinos (\_ -> [])
+-- Crea un Grafo con
+-- ningun nodo ([]), y
+-- una funcion de vecindad que a cualquier posible nodo asocia una lista vacia de vecinos (\_ -> [])
 vacio :: Grafo a
 vacio = G [] (\_ -> [])
 
@@ -25,32 +27,26 @@ vecinos (G ns fv) = fv
 
 -- Ejercicio 4
 agNodo :: Eq a => a -> Grafo a -> Grafo a
-agNodo n (G ns fv) 	
-	| n `elem` ns 	= 	(G ns fv)
-	| otherwise 	=	(G (n:ns) fv)
+agNodo n (G ns fv) = G (List.union [n] ns) fv
 
 -- Ejercicio 5
 sacarNodo :: Eq a => a -> Grafo a -> Grafo a
-sacarNodo n (G ns fv)	
-	= 	(G (removerElem ns) (\e -> removerElem (fv e))) --quitar los de ns
-		where removerElem =  (filter (\e -> e /= n))
+sacarNodo n (G ns fv) = G (List.delete n ns) (List.delete n . fv)
 
 -- Ejercicio 6
 agEje :: Eq a =>  (a,a) -> Grafo a -> Grafo a
-agEje (n1,n2) (G ns fv) = (G ns (\e -> if e == n1 then n2:(fv e) else fv e) )
+agEje (n1, n2) (G ns fv) = G ns (\e -> if e == n1 then (n2:fv e) else (fv e)) 
 
 -- Ejercicio 7
-lineal :: Eq a =>  [a] -> Grafo a
-lineal ns = (G ns (\n ->  cabeza ( tail ( dropWhile (/= n) ns ) )  ) )
+lineal :: Eq a => [a] -> Grafo a
+lineal ns = G ns (\n -> take 1 (tail (dropWhile (/= n) ns))) 
 
-cabeza [] = []
-cabeza (x:xs) = [x]
+-- Sin lambda:
+-- lineal ns = G ns (take 1 . tail . flip dropWhile ns . (/=))
 
 -- Ejercicio 8
 union :: Eq a => Grafo a -> Grafo a -> Grafo a
-union (G ns1 fv1) (G ns2 fv2) 
-	= ( G (sinRepetidos (ns1 ++ ns2)) (\e -> (sinRepetidos (fv1 e) ++ (fv2 e) ) ) )
-	where sinRepetidos = foldr (\n rec -> if n `notElem` rec then n:rec else rec) []
+union (G ns1 fv1) (G ns2 fv2) = G (List.union ns1 ns2) (\e -> (List.union (fv1 e) (fv2 e)))
 	
 -- Ejercicio 9
 clausura :: Grafo a -> Grafo a
