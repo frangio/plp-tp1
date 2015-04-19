f3150faacda78a7db23910296dc02ea5f36e9acd
Francisco Giordano <frangio.1@gmail.com>
2015-04-19 00:43:27 -0300

    indentación

--- Lomoba.hs
+++ Lomoba.hs
@@ -8,49 +8,45 @@ import qualified Data.List as List (delete, union, nub)
 
 -- Ejercicio 10
 foldExp :: (Prop -> b) -> (b -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b) -> (b -> b) -> Exp -> b
-foldExp fVar fNot fOr fAnd fD fB ei
-	= let frec = foldExp fVar fNot fOr fAnd fD fB in
-	case ei of
-	Var p -> fVar p
-	Not e -> fNot (frec e)
-	Or e1 e2 -> fOr (frec e1) (frec e2) 
-	And e3 e4 -> fAnd (frec e3) (frec e4) 
-	D e5 -> fD (frec e5) 
-	B e6 -> fB (frec e6)
+foldExp fVar fNot fOr fAnd fD fB ei =
+  let frec = foldExp fVar fNot fOr fAnd fD fB
+  in case ei of
+    Var p -> fVar p
+    Not e -> fNot (frec e)
+    Or e1 e2 -> fOr (frec e1) (frec e2)
+    And e3 e4 -> fAnd (frec e3) (frec e4)
+    D e5 -> fD (frec e5)
+    B e6 -> fB (frec e6)
 
 -- Ejercicio 11
 visibilidad :: Exp -> Integer
 visibilidad = foldExp fVar fNot fOr fAnd fD fB
-	where  
-		fVar = const 0
-		fNot = id
-		fOr = max
-		fAnd = max
-		fD = (1+)
-		fB = (1+)
-	
+  where fVar = const 0
+        fNot = id
+        fOr = max
+        fAnd = max
+        fD = (1+)
+        fB = (1+)
 
 -- Ejercicio 12
 extraer :: Exp -> [Prop]
 extraer = List.nub . foldExp fVar fNot fOr fAnd fD fB
-	where  
-		fVar = (:[])
-		fNot = id 
-		fOr = (++)
-		fAnd = (++)
-		fD = id
-		fB = id
+  where fVar = (:[])
+        fNot = id
+        fOr = (++)
+        fAnd = (++)
+        fD = id
+        fB = id
 
 -- Ejercicio 13
 eval :: Modelo -> Mundo -> Exp -> Bool
 eval m@(K g v) w e = foldExp fVar fNot fOr fAnd fD fB e
-	where
-		fVar x = elem w (v x)
-		fNot = not
-		fOr = (||)
-		fAnd = (&&)
-		fD _ = any (\w' -> eval m w' e) (vecinos g w)
-		fB _ = all (\w' -> eval m w' e) (vecinos g w)
+  where fVar x = elem w (v x)
+        fNot = not
+        fOr = (||)
+        fAnd = (&&)
+        fD _ = any (\w' -> eval m w' e) (vecinos g w)
+        fB _ = all (\w' -> eval m w' e) (vecinos g w)
 
 -- Ejercicio 14
 --duda acá : tengo que devolver los mundos del modelo para los que vale la fórmula?
